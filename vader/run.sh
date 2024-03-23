551d7b1b2cd4af8643e117d2cd4bf99cc60fbd7e
tenfyzhong <tenfy@tenfy.cn>
2024-03-23 14:09:47 +0800

    vader action

---
+++ vader/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+export tmp=$(mktemp -d)
+export PLUGINS="junegunn/vader:$PLUGINS"
+
+cat <<EOF > .vimrc
+filetype off
+set rtp+=.
+set rtp+=vader.vim
+EOF
+
+old_ifs="$IFS"
+IFS=':'
+for plugin in $PLUGINS; do
+    git clone --depth 1 "https://github.com/$plugin.git" "$tmp/$plugin"
+    echo "set rtp+=$tmp/$plugin" >> .vimrc
+done
+IFS="$old_ifs"
+
+
+cat <<EOF >> .vimrc
+filetype plugin indent on
+syntax enable
+EOF
+
+"$EDITOR" -Es -u .vimrc -c "Vader! $TEST_PATTERN"
