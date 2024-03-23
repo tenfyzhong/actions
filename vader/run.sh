0a7d0068115dd4c5238265db71b78c4c4dc51f6e
tenfy <tenfy@tenfy.cn>
2024-03-23 21:51:25 +0800

    vader (#1)
    * vader action
    
    * enable nvim
    
    * fix clone plugins
    
    * fix action rtp
    
    * fix vader url
    
    * clone plugins
    
    * set gh token
    
    * echo
    
    * add sshx to debug
    
    * fix vim vader
    
    * set rtp to vimrc
    
    * vader readme

---
+++ vader/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export tmp=$(mktemp -d)
+PLUGINS="junegunn/vader.vim:$PLUGINS"
+cwd=$(pwd)
+
+cat <<EOF > .vimrc
+filetype off
+set nocompatible
+EOF
+
+echo "set rtp+=$cwd" >> .vimrc
+
+old_ifs="$IFS"
+IFS=':'
+for plugin in $PLUGINS; do
+    path="$tmp/$plugin"
+    echo "Cloning $plugin to $path"
+    gh repo clone "$plugin" "$path"
+    echo "set rtp+=$path" >> .vimrc
+done
+IFS="$old_ifs"
+
+
+cat <<EOF >> .vimrc
+filetype plugin indent on
+syntax enable
+EOF
+
+echo 'Running vader'
+"$EDITOR" -Es -u .vimrc -c "Vader! $TEST_PATTERN"
